4f066ff64af1aa07b1aa3ea1f89f3dbb04338621
Ignacio Cisternas <105437306+nachodev-ui@users.noreply.github.com>
2025-04-08 15:07:33 -0400

    Update streamlit_app(1).py
    Parse to JSON API info

--- streamlit_app(1).py
+++ streamlit_app(1).py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 st.title("Customize your smoothie")
 st.write("Choose the fruits you want in your custom Smoothie")
@@ -38,7 +39,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success(f'Your Smoothie is ordered, {name_on_order}! ✅')
-
-import requests
+        
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
