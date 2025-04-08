e7c8f55516dd8197dce773e7d0226974fcc9dfdd
Ignacio Cisternas <105437306+nachodev-ui@users.noreply.github.com>
2025-04-08 15:12:38 -0400

    Update streamlit_app(1).py

--- streamlit_app(1).py
+++ streamlit_app(1).py
@@ -41,5 +41,4 @@ if ingredients_list:
         st.success(f'Your Smoothie is ordered, {name_on_order}! ✅')
         
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
